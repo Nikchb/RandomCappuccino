78a7a08492d7641f81b114776fd5fed632084309
Nikita Chubko <cekytor.007@gmail.com>
2021-09-24 20:37:05 +0200

    Participant manager created

--- RandomCappuccino.Server/Services/ParticipantManager/IParticipantManager.cs
+++ RandomCappuccino.Server/Services/ParticipantManager/IParticipantManager.cs
@@ -6,14 +6,14 @@ namespace RandomCappuccino.Server.Services.ParticipantManager
 {
     public interface IParticipantManager
     {
-        Task<ServiceContentResponse<ParticipantDTO>> CreateParticipant(string userId, CreateParticipantDTO model);
+        Task<ServiceContentResponse<ParticipantDTO>> CreateParticipant(CreateParticipantDTO model);
 
-        Task<ServiceContentResponse<ParticipantDTO>> GetParticipant(string userId, string partcipantId);
+        Task<ServiceContentResponse<ParticipantDTO>> GetParticipant(string partcipantId);
 
-        Task<ServiceContentResponse<IEnumerable<ParticipantDTO>>> GetGroupParticipants(string userId, string groupId);
+        Task<ServiceContentResponse<IEnumerable<ParticipantDTO>>> GetGroupParticipants(string groupId);
 
-        Task<ServiceContentResponse<ParticipantDTO>> UpdateParticipant(string userId, ParticipantDTO model);
+        Task<ServiceResponse> UpdateParticipant(ParticipantDTO model);
 
-        Task<ServiceResponse> DeleteParticipant(string userId, string participantId);
+        Task<ServiceResponse> DeleteParticipant(string participantId);
     }
 }
--- RandomCappuccino.Server/Services/ParticipantManager/ParticipantManager.cs
+++ RandomCappuccino.Server/Services/ParticipantManager/ParticipantManager.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
-
+using Microsoft.EntityFrameworkCore;
 using RandomCappuccino.Server.Data;
 using RandomCappuccino.Server.Data.Models;
 using RandomCappuccino.Server.Services.GroupManager;
+using RandomCappuccino.Server.Services.IdentityManager;
 using RandomCappuccino.Server.Services.ParticipantManager.DTOs;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RandomCappuccino.Server.Services.ParticipantManager
@@ -14,18 +16,24 @@ namespace RandomCappuccino.Server.Services.ParticipantManager
         private readonly IMapper mapper;
         private readonly DataBaseContext context;
         private readonly IGroupManager groupManager;
+        private readonly IIdentityManager identityManager;
 
-        public ParticipantManager(IMapper mapper, DataBaseContext context, IGroupManager groupManager)
+        public ParticipantManager(IMapper mapper, DataBaseContext context, IGroupManager groupManager, IIdentityManager identityManager)
         {
             this.mapper = mapper;
             this.context = context;
             this.groupManager = groupManager;
+            this.identityManager = identityManager;
         }
 
-        public async Task<ServiceContentResponse<ParticipantDTO>> CreateParticipant(string userId, CreateParticipantDTO model)
+        public async Task<ServiceContentResponse<ParticipantDTO>> CreateParticipant(CreateParticipantDTO model)
         {
+            var groupResponse = await groupManager.GetGroup(model.GroupId);
+            if(groupResponse.Succeed == false)
+            {
+                return Decline<ParticipantDTO>(groupResponse.Messages);
+            } 
             
-
             var participant = mapper.Map<Participant>(model);
 
             try
@@ -41,28 +49,97 @@ namespace RandomCappuccino.Server.Services.ParticipantManager
             return Accept(mapper.Map<ParticipantDTO>(participant));
         }
 
-        public async Task<ServiceResponse> DeleteParticipant(string userId, string participantId)
+        public async Task<ServiceResponse> DeleteParticipant(string participantId)
         {
+            var participant = await context.Participants.FindAsync(participantId);
+            if (participant == null ? true : participant.IsActive == false)
+            {
+                Decline("Participant is not found");
+            }
 
+            var groupResponse = await groupManager.GetGroup(participant.GroupId);
+            if (groupResponse.Succeed == false)
+            {
+                return Decline(groupResponse.Messages);
+            }
 
-            throw new System.NotImplementedException();
+            participant.IsActive = false;
 
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch
+            {
+                return Decline("Participant delete is failed");
+            }
 
+            return Accept();
         }
 
-        public Task<ServiceContentResponse<IEnumerable<ParticipantDTO>>> GetGroupParticipants(string userId, string groupId)
+        public async Task<ServiceContentResponse<IEnumerable<ParticipantDTO>>> GetGroupParticipants(string groupId)
         {
-            throw new System.NotImplementedException();
+            var groupResponse = await groupManager.GetGroup(groupId);
+            if (groupResponse.Succeed == false)
+            {
+                return Decline<IEnumerable<ParticipantDTO>>(groupResponse.Messages);
+            }
+
+            try
+            {
+                var participants = await context.Participants.Where(v => v.GroupId == groupId && v.IsActive)
+                                                             .Select(v => mapper.Map<ParticipantDTO>(v)).ToArrayAsync();
+                return Accept(participants.AsEnumerable());
+            }
+            catch
+            {
+                return Decline<IEnumerable<ParticipantDTO>>("Participant request is failed");
+            }
         }
 
-        public Task<ServiceContentResponse<ParticipantDTO>> GetParticipant(string userId, string partcipantId)
+        public async Task<ServiceContentResponse<ParticipantDTO>> GetParticipant(string partiicpantId)
         {
-            throw new System.NotImplementedException();
+            var participant = await context.Participants.FindAsync(partiicpantId);
+            if (participant == null ? true : participant.IsActive == false)
+            {
+                Decline<ParticipantDTO>("Participant is not found");
+            }
+
+            var groupResponse = await groupManager.GetGroup(participant.GroupId);
+            if (groupResponse.Succeed == false)
+            {
+                return Decline<ParticipantDTO>(groupResponse.Messages);
+            }
+
+            return Accept(mapper.Map<ParticipantDTO>(participant));
         }
 
-        public Task<ServiceContentResponse<ParticipantDTO>> UpdateParticipant(string userId, ParticipantDTO model)
+        public async Task<ServiceResponse> UpdateParticipant(ParticipantDTO model)
         {
-            throw new System.NotImplementedException();
+            var participant = await context.Participants.FindAsync(model.Id);
+            if (participant == null ? true : participant.IsActive == false)
+            {
+                Decline("Participant is not found");
+            }
+
+            var groupResponse = await groupManager.GetGroup(participant.GroupId);
+            if (groupResponse.Succeed == false)
+            {
+                return Decline(groupResponse.Messages);
+            }
+
+            mapper.Map(model, participant);
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch
+            {
+                return Decline("Participant update is failed");
+            }
+
+            return Accept();
         }
     }
 }
